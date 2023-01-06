ef4ae396f37d4741c73c5709a781697a58b9a6e1
devsilva <jerson.silva99@hotmail.com>
2023-01-05 22:27:00 -0500

    feat: Add icons for the forms

---
+++ components/Icons/Back.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Back = (props) => (
+  <svg
+    className="tiktok-1bhojfp-StyledBackIcon e1x3uhsi2"
+    width="1em"
+    height="1em"
+    viewBox="0 0 48 48"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M4.586 22.586 20.879 6.293a1 1 0 0 1 1.414 0l1.414 1.414a1 1 0 0 1 0 1.414L8.828 24l14.88 14.879a1 1 0 0 1 0 1.414l-1.415 1.414a1 1 0 0 1-1.414 0L4.586 25.414a2 2 0 0 1 0-2.828Z"
+    />
+  </svg>
+)
+
+export default Back
---
+++ components/Icons/Google.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Google = (props) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 48 48"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M43 24.431a21.4 21.4 0 0 0-.352-3.886h-18.26v7.35h10.434c-.45 2.375-1.816 4.387-3.869 5.734v4.767h6.266C40.885 35.09 43 30.218 43 24.431Z"
+      fill="#4285F4"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M24.387 43.001c5.235 0 9.624-1.701 12.831-4.603l-6.265-4.768c-1.736 1.14-3.957 1.814-6.566 1.814-5.05 0-9.324-3.342-10.848-7.833H7.062v4.923C10.252 38.744 16.808 43 24.387 43Z"
+      fill="#34A853"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M13.54 27.61A11.21 11.21 0 0 1 12.93 24c0-1.253.22-2.47.609-3.61v-4.923H7.062a18.684 18.684 0 0 0 0 17.065l6.478-4.923Z"
+      fill="#FBBC04"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M24.387 12.557c2.847 0 5.402.958 7.412 2.841l5.56-5.45C34.002 6.884 29.613 5 24.387 5 16.808 5 10.252 9.258 7.062 15.467l6.477 4.923c1.524-4.49 5.798-7.833 10.848-7.833Z"
+      fill="#EA4335"
+    />
+  </svg>
+)
+
+export default Google
