0b3fcb785660d2e14b998f5da295aab20381b60c
devsilva <jerson.silva99@hotmail.com>
2023-01-05 23:10:56 -0500

    feat: Create login page

---
+++ pages/login/index.js
@@ -0,0 +1,76 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+
+import User from 'components/Icons/User'
+import Apple from 'components/Icons/Apple'
+import Google from 'components/Icons/Google'
+import Github from 'components/Icons/Github'
+import Twitter from 'components/Icons/Twitter'
+import Facebook from 'components/Icons/Facebook'
+
+import styles from './styles.module.css'
+
+const Login = () => {
+    const router = useRouter()
+
+    const handleSignup = () => {
+        router.push('/signup')
+    }
+    return <>
+        <Head>
+            <title>Iniciar sesión | TikTok</title>
+            <meta name='description' content='TikTok Clone next.js firebase' />
+            <link rel='icon' href='favicon.png' />
+        </Head>
+        <div className='app'>
+            <section className={styles.login__container}>
+                <h1 className={styles.login__title}>Iniciar sesión en TikTok</h1>
+                <h2 className={styles.login__subtitle}>Gestiona tu cuenta, lee notificaciones, comenta sobre vídeos y más</h2>
+                <div className={styles.login__with}>
+                    <div className={styles.login__icon}>
+                        <User />
+                    </div>
+                    <p>Usar teléfono/correo/nombre de usuario</p>
+                </div>
+                <div className={styles.login__with}>
+                    <div className={styles.login__icon}>
+                        <Facebook />
+                    </div>
+                    <p>Continuar con Facebook</p>
+                </div>
+                <div className={styles.login__with}>
+                    <div className={styles.login__icon}>
+                        <Google />
+                    </div>
+                    <p>Continuar con Google</p>
+                </div>
+                <div className={styles.login__with}>
+                    <div className={styles.login__icon}>
+                        <Twitter />
+                    </div>
+                    <p>Continuar con Twitter</p>
+                </div>
+                <div className={styles.login__with}>
+                    <div className={styles.login__icon}>
+                        <Apple />
+                    </div>
+                    <p>Continuar con Apple</p>
+                </div>
+                <div className={styles.login__with}>
+                    <div className={styles.login__icon}>
+                        <Github />
+                    </div>
+                    <p>Continuar con Github</p>
+                </div>
+            </section>
+            <div className={styles.footer__container}>
+                <div className={styles.text__login}>¿No tienes cuenta?</div>
+                <a onClick={handleSignup} className={styles.link__login}>
+                    <span className={styles.bottom__login}>Registrarse</span>
+                </a>
+            </div>
+        </div>
+    </>
+}
+
+export default Login
