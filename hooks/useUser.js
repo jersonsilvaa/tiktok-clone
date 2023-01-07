bb655f3dcb3376eeeb095a3ec33189e567907e9f
devsilva <jerson.silva99@hotmail.com>
2023-01-07 02:52:52 -0500

    feat: Create state for auth

---
+++ hooks/useUser.js
@@ -0,0 +1,25 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+import { authStateChanged } from 'services/client'
+
+export const STATES = {
+    NOT_LOGGED: null,
+    NOT_KNOWN: undefined
+}
+
+const useUser = () => {
+    const [user, setUser] = useState(STATES.NOT_KNOWN)
+    
+    const router = useRouter()
+
+    useEffect(() => {
+        authStateChanged(setUser)
+    }, [])
+
+    useEffect(() => {
+        user === STATES.NOT_LOGGED && router.push('/login')
+    }, [user])
+}
+
+export default useUser
