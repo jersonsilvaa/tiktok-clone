f62b6b61af04d407fd30041453b2f0b913134e6b
devsilva <jerson.silva99@hotmail.com>
2023-01-07 02:53:59 -0500

    feat: Home page

---
+++ pages/home/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const Home = () => {
+    return <>
+        <Head>
+            <title>Mira vídeos populares seleccionados para ti | TikTok</title>
+            <meta name='description' content='TikTok Clone next.js firebase' />
+            <link rel='icon' href='favicon.png' />
+        </Head>
+    </>
+}
+
+export default Home
