cbc7581939328df9b8b2941fec755e7a4e9d7cbc
devsilva <jerson.silva99@hotmail.com>
2023-01-08 17:56:29 -0500

    feat: Make signin and use hook for detect login

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 import User from 'components/Icons/User'
@@ -6,17 +7,42 @@ import Google from 'components/Icons/Google'
 import Github from 'components/Icons/Github'
 import Facebook from 'components/Icons/Facebook'
 
-import {
-  signInWithGoogle,
-  signInWithGithub,
-  signInWithFacebook
-} from 'services/client'
+import useUser, { STATES } from 'hooks/useUser'
+
+import { loginWithFacebook, loginWithGoogle, loginWithGithub } from 'services/client'
 
 const Home = () => {
+  const user = useUser()
   const router = useRouter()
+
+  useEffect(() => {
+    user && router.replace('/home')
+  }, [user])
+
 
   const redirectEmail = () => {
     router.push('/email')
+  }
+
+  const handleLoginFacebook = () => {
+    loginWithFacebook()
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  const handleLoginGoogle = () => {
+    loginWithGoogle()
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  const handleLoginGithub = () => {
+    loginWithGithub()
+      .catch(err => {
+        console.error(err)
+      })
   }
   return <>
     <Head>
@@ -28,30 +54,35 @@ const Home = () => {
       <section className='login__container'>
         <h1 className='login__title'>Iniciar sesión en TikTok</h1>
         <h2 className='login__subtitle'>Gestiona tu cuenta, lee notificaciones, comenta sobre vídeos y más</h2>
-        <div onClick={redirectEmail} className='login__with'>
-          <div className='login__icon'>
-            <User />
-          </div>
-          <p>Usar teléfono/correo/nombre de usuario</p>
-        </div>
-        <div onClick={signInWithFacebook} className='login__with'>
-          <div className='login__icon'>
-            <Facebook />
-          </div>
-          <p>Continuar con Facebook</p>
-        </div>
-        <div onClick={signInWithGoogle} className='login__with'>
-          <div className='login__icon'>
-            <Google />
-          </div>
-          <p>Continuar con Google</p>
-        </div>
-        <div onClick={signInWithGithub} className='login__with'>
-          <div className='login__icon'>
-            <Github />
-          </div>
-          <p>Continuar con Github</p>
-        </div>
+        {
+          user === STATES.NOT_LOGGED && <>
+            <div onClick={redirectEmail} className='login__with'>
+              <div className='login__icon'>
+                <User />
+              </div>
+              <p>Usar teléfono/correo/nombre de usuario</p>
+            </div>
+            <div onClick={handleLoginFacebook} className='login__with'>
+              <div className='login__icon'>
+                <Facebook />
+              </div>
+              <p>Continuar con Facebook</p>
+            </div>
+            <div onClick={handleLoginGoogle} className='login__with'>
+              <div className='login__icon'>
+                <Google />
+              </div>
+              <p>Continuar con Google</p>
+            </div>
+            <div onClick={handleLoginGithub} className='login__with'>
+              <div className='login__icon'>
+                <Github />
+              </div>
+              <p>Continuar con Github</p>
+            </div>
+          </>
+        }
+        {user === STATES.NOT_KNOWN && <img src='spinner.gif' />}
       </section>
     </div>
   </>
