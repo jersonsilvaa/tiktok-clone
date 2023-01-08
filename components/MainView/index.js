b93306c882390bf943908c10103eb6fc82bef37b
devsilva <jerson.silva99@hotmail.com>
2023-01-08 17:53:22 -0500

    feat: Add main content

---
+++ components/MainView/index.js
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+const MainView = () => {
+    return <>
+    <div className='app'>
+        <h1>Contenido principal</h1>
+    </div>
+    </>
+}
+
+export default MainView
