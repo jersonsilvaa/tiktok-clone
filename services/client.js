a92b1ee986aa4ff610d225a1f8f56acdeb50f01f
devsilva <jerson.silva99@hotmail.com>
2023-01-08 18:09:44 -0500

    refactor: Change mode of the sigin

--- services/client.js
+++ services/client.js
@@ -1,74 +1,59 @@
 import { initializeApp } from 'firebase/app'
 
-import { getStorage } from 'firebase/storage'
-import { getFirestore } from 'firebase/firestore'
 import {
-    getAuth,
-    signInWithPopup,
-    EmailAuthProvider,
-    GoogleAuthProvider,
-    GithubAuthProvider,
-    FacebookAuthProvider,
-    onAuthStateChanged
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  onAuthStateChanged,
+  FacebookAuthProvider
 } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APPID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID
 }
 
 const app = initializeApp(firebaseConfig)
+
 const auth = getAuth(app)
-const db = getFirestore()
 
+const mapUserFromFirebaseAuth = (user) => {
+  user = auth.currentUser
 
-const mapUserFirebaseAuth = () => {
-    const user = auth.currentUser
+  const username = user.displayName
+  const avatar = user.photoURL
+  const uid = user.uid
 
-    const username = user.displayName
-    const photoURL = user.photoURL
-    const uid = user.uid
-
-    return {
-        username,
-        photoURL,
-        uid
-    }
+  return {
+    username,
+    avatar,
+    uid
+  }
 }
 
-// Signup with
-
-export const signInWithGoogle = () => {
-    const googleProvider = new GoogleAuthProvider()
-    signInWithPopup(auth, googleProvider)
-        .then(() => {
-            mapUserFirebaseAuth()
-        })
+export const authStateChanged = (onChange) => {
+  return onAuthStateChanged(auth, user => {
+    const normalizedUser = user ? mapUserFromFirebaseAuth(user) : null
+    onChange(normalizedUser)
+  })
 }
 
-export const signInWithGithub = () => {
-    const githubProvider = new GithubAuthProvider()
-    signInWithPopup(auth, githubProvider)
-        .then(() => {
-            mapUserFirebaseAuth()
-        })
+export const loginWithGithub = () => {
+  const githubProvider = new GithubAuthProvider()
+  return signInWithPopup(auth, githubProvider)
 }
 
-export const signInWithFacebook = () => {
-    const facebookProvider = new FacebookAuthProvider()
-    signInWithPopup(auth, facebookProvider)
-        .then(() => {
-            mapUserFirebaseAuth()
-        })
+export const loginWithGoogle = () => {
+  const googleProvider = new GoogleAuthProvider()
+  return signInWithPopup(auth, googleProvider)
 }
 
-export const authStateChanged = onchange => {
-    return onAuthStateChanged(auth, (user) => {
-        const normalizeUser = user ? mapUserFirebaseAuth(user) : null
-        onchange(normalizeUser)
-    })
+export const loginWithFacebook = () => {
+  const facebookProvider = new FacebookAuthProvider()
+  return signInWithPopup(auth, facebookProvider)
 }
