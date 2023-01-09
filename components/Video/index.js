2642dda15d0d6def80021b2c16ffa39680fbcfb8
devsilva <jerson.silva99@hotmail.com>
2023-01-09 15:06:20 -0500

    feat: Add handle click for play and puase video

--- components/Video/index.js
+++ components/Video/index.js
@@ -1,6 +1,7 @@
+import clsx from 'clsx'
 import { useRef, useState } from 'react'
 
-import style from './styles.module.css'
+import styles from './styles.module.css'
 
 const Video = ({ src }) => {
     const [playing, setPlaying] = useState(false)
@@ -8,25 +9,28 @@ const Video = ({ src }) => {
     const videoRef = useRef(null)
 
     const handleClick = () => {
-        if (playing) {
-            videoRef.current.pause()
-            setPlaying(false)
-        } else {
-            videoRef.current.play()
-            setPlaying(true)
-        }
+        const { current: videoEl } = videoRef
+
+        playing ? videoEl.play() : videoEl.pause()
+        
+        setPlaying(!playing)
     }
 
+    const playClassName = clsx(styles.player, {
+        [styles.hidden]: !playing
+    })
+
     return (
-        <div className={style.wrapper}>
+        <div className={styles.wrapper}>
             <video
-                className={style.videoPlayer}
                 loop
-                onClick={handleClick}
                 ref={videoRef}
                 src={src}
                 style={{ objectFit: 'cover' }}
+                onClick={handleClick}
+                className={styles.videoPlayer}
             />
+            <button onClick={handleClick} className={playClassName} />
         </div>
     )
 }
